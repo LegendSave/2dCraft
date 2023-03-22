644ca65917265182a766b7e3236f4b4f6881195b
LegendAmber <6006082@stu.kingsburghigh.com>
2023-03-21 19:04:22 -0700

    Basic formatting
    This formatted the page, which will be used for the final UI.

---
+++ Assets/CanvasFullscreen.js
@@ -0,0 +1,27 @@
+let canvas = document.querySelector("canvas");
+function fullscreen() {
+    if(document.fullscreenElement){
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+          } else if (document.mozCancelFullScreen) { /* Firefox */
+            document.mozCancelFullScreen();
+          }          
+    }
+    else if(document.mozFullScreenElement){
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+          } else if (document.mozCancelFullScreen) { /* Firefox */
+            document.mozCancelFullScreen();
+          }          
+    }else{
+    canvas.requestFullscreen({navigationUI: "auto"});
+}
+} 
